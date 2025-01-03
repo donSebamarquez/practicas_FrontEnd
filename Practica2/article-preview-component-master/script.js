7e5de6a7fd89281eea73430e739fa144e78f5134
sebamarques <sebamarquezblanco@gmail.com>
2025-01-03 20:15:43 -0300

    retoques

--- Practica2/article-preview-component-master/script.js
+++ Practica2/article-preview-component-master/script.js
@@ -3,7 +3,15 @@ let share = document.getElementById("yqc1");
 let active = document.getElementById("yqc2"); 
 
 boton.addEventListener("click", function() {
-    share.classList.toggle("deactivate"); 
-    active.classList.toggle("deactivate");   
+    
+    if(window.screen.width<500){
+        share.classList.toggle("deactivate"); 
+        active.classList.toggle("deactivate");
+    }
+    if(window.screen.width>500){
+        share.classList.toggle("deactivate");
+        active.classList.toggle("deactivate");   
+        
+    }
     boton.classList.toggle("active");
 });
