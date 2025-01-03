f186805e19b02be4f59bbb57ae5485f0138d3999
sebamarques <sebamarquezblanco@gmail.com>
2025-01-02 22:34:21 -0300

    script

--- Practica2/article-preview-component-master/script.js
+++ Practica2/article-preview-component-master/script.js
@@ -1,5 +1,9 @@
-let boton = document.getElementById("share")
+let boton = document.getElementById("share");
+let share = document.getElementById("yqc1"); 
+let active = document.getElementById("yqc2"); 
 
-boton.addEventListener("click",function(){
-    console.log("hola soy el boton")
-})
+boton.addEventListener("click", function() {
+    share.classList.toggle("deactivate"); 
+    active.classList.toggle("deactivate");   
+    boton.classList.toggle("active");
+});
