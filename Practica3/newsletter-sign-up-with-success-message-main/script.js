67582c49c020e2c3e78fafed009f4faccf806a6d
sebamarques <sebamarquezblanco@gmail.com>
2025-01-05 21:11:49 -0300

    2daPracticaJS

--- Practica3/newsletter-sign-up-with-success-message-main/script.js
+++ Practica3/newsletter-sign-up-with-success-message-main/script.js
@@ -5,6 +5,7 @@ let primary = document.getElementById("primary");
 let secondary = document.getElementById("secondary");
 let err_message = document.getElementById("err_msg");
 let user = document.getElementById("userEmail");
+let input = document.getElementById("input")
 const form = document.getElementById('form');
 
 
@@ -15,27 +16,33 @@ function handleSubmit(e) {
 form.addEventListener('submit', handleSubmit);
 
 button.addEventListener("click",()=>{
-    let valor_input = document.getElementById("input").value
+    let valor_input = input.value
     let addUser = ``
     if(validEmail.test(valor_input)){
 
         primary.classList.add("deactivate");
+        primary.classList.remove("card__primary");
         secondary.classList.remove("deactivate");
+        secondary.classList.add("card__secondary");
         err_message.classList.add("deactivate");
+        input.classList.remove("err")
         addUser =  `${valor_input}`;
         user.innerHTML = addUser;
     }   
 
     else{
         primary.classList.remove("deactivate");
-        secondary.classList.add("deactivate")
+        secondary.classList.add("deactivate");
         err_message.classList.remove("deactivate");
+        input.classList.add("err");
+        
     }
-    valor_input = "";
-    addUser = ``;
 })
 
 button2.addEventListener("click",()=>{
+    primary.classList.add("card__primary");
     primary.classList.remove("deactivate");
     secondary.classList.add("deactivate");
+    secondary.classList.remove("card__secondary");
+    input.classList.remove("err");
 })
