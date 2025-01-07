646aebac29bf6eb0337ca7a7a90e2c50d7b3f594
sebamarques <sebamarquezblanco@gmail.com>
2025-01-07 13:24:31 -0300

    practica3Ejercicio3

--- Practica3/time-tracking-dashboard-main/script.js
+++ Practica3/time-tracking-dashboard-main/script.js
@@ -6,22 +6,22 @@ let valoresTraidos = fetch("http://192.168.1.11:5500/Practica3/time-tracking-das
 .then(response => response.json())
 .then(data => {
     values = data
-    seleccion('daily');
+    seleccion('day');
 });
 
 
 
-function seleccion(value){
-   if(value === 'daily'){
+function seleccion(fecha){
+   if(fecha === 'day'){
         values.map(value=>{
             valoresSeleccionados += `
-                <div class= "card__values">
+                <div class= "card__values ${value.id}">
                     <div class="row">
                         <div class="column">
-                            <h3 class="value__title">${value.title}</h3>
-                            <p class="value__current">${value.timeframes.daily.current}</p>
+                            <p class="value__title">${value.title}</p>
+                            <p class="value__current">${value.timeframes.day.current}hrs</p>
                         </div>
-                        <p class="value__previous">Last week - ${value.timeframes.daily.previous}</p>
+                        <p class="value__previous">Last ${fecha} - ${value.timeframes.day.previous}hrs</p>
                     
                     </div>
                     
@@ -31,16 +31,16 @@ function seleccion(value){
         mostrarValores.innerHTML = valoresSeleccionados;
         valoresSeleccionados = ``;
    }
-   if(value === 'weekly'){
+   if(fecha === 'week'){
         values.map(value=>{
             valoresSeleccionados += `
-                <div class="card__values">
+                <div class="card__values ${value.id}">
                     <div class="row">
                         <div class="column">
-                            <h3 class="value__title">${value.title}</h3>
-                            <p class="value__current">${value.timeframes.weekly.current}</p>
+                            <p class="value__title">${value.title}</p>
+                            <p class="value__current">${value.timeframes.week.current}hrs</p>
                         </div>
-                        <p class="value__previous">Last week - ${value.timeframes.weekly.previous}</p>
+                        <p class="value__previous">Last ${fecha} - ${value.timeframes.week.previous}hrs</p>
                     </div>
                 </div>
             `
@@ -48,16 +48,16 @@ function seleccion(value){
         mostrarValores.innerHTML = valoresSeleccionados;
         valoresSeleccionados = ``;
    }
-   if(value === 'monthly'){
+   if(fecha === 'month'){
         values.map(value=>{
             valoresSeleccionados += `
-                <div class="card__values">
+                <div class="card__values ${value.id}">
                     <div class="row">
                         <div class="column">
-                            <h3 class="value__title">${value.title}</h3>
-                            <p class="value__current">${value.timeframes.monthly.current}</p>
+                            <p class="value__title">${value.title}</p>
+                            <p class="value__current">${value.timeframes.month.current}hrs</p>
                         </div>
-                        <p class="value__previous">Last week - ${value.timeframes.monthly.previous}</p>
+                        <p class="value__previous">Last ${fecha} - ${value.timeframes.month.previous}hrs</p>
                     </div>
                 </div>
             `
