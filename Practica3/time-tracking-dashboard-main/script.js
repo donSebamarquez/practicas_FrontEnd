ff201c58a8658ed37e1bf9be547f71fd6da6ef0b
sebamarques <sebamarquezblanco@gmail.com>
2025-01-15 18:43:25 -0300

    Eliminar DRY

--- Practica3/time-tracking-dashboard-main/script.js
+++ Practica3/time-tracking-dashboard-main/script.js
@@ -12,57 +12,21 @@ let valorTraido = fetch("https://raw.githubusercontent.com/sebamarques/practicas
 
 
 function seleccion(fecha){
-   if(fecha === 'day'){
-        values.map(value=>{
-            valoresSeleccionados += `
-                <div class= "card__values ${value.id}">
-                    <div class="row">
-                        <div class="column">
-                            <p class="value__title">${value.title}</p>
-                            <p class="value__current">${value.timeframes.day.current}hrs</p>
-                        </div>
-                        <p class="value__previous">Last ${fecha} - ${value.timeframes.day.previous}hrs</p>
-                    
-                    </div>
-                    
-                </div>
-            `
-        })
-        mostrarValores.innerHTML = valoresSeleccionados;
-        valoresSeleccionados = ``;
-   }
-   if(fecha === 'week'){
-        values.map(value=>{
-            valoresSeleccionados += `
-                <div class="card__values ${value.id}">
-                    <div class="row">
-                        <div class="column">
-                            <p class="value__title">${value.title}</p>
-                            <p class="value__current">${value.timeframes.week.current}hrs</p>
-                        </div>
-                        <p class="value__previous">Last ${fecha} - ${value.timeframes.week.previous}hrs</p>
-                    </div>
-                </div>
-            `
-        })
-        mostrarValores.innerHTML = valoresSeleccionados;
-        valoresSeleccionados = ``;
-   }
-   if(fecha === 'month'){
-        values.map(value=>{
-            valoresSeleccionados += `
-                <div class="card__values ${value.id}">
-                    <div class="row">
-                        <div class="column">
-                            <p class="value__title">${value.title}</p>
-                            <p class="value__current">${value.timeframes.month.current}hrs</p>
-                        </div>
-                        <p class="value__previous">Last ${fecha} - ${value.timeframes.month.previous}hrs</p>
-                    </div>
-                </div>
-            `
-        })
-        mostrarValores.innerHTML = valoresSeleccionados;
-        valoresSeleccionados = ``;
-    }
+    valoresSeleccionados = '';
+
+    values.forEach(value => {
+      valoresSeleccionados += `
+        <div class="card__values ${value.id}">
+          <div class="row">
+            <div class="column">
+              <p class="value__title">${value.title}</p>
+              <p class="value__current">${value.timeframes[fecha].current}hrs</p>
+            </div>
+            <p class="value__previous">Last ${fecha} - ${value.timeframes[fecha].previous}hrs</p>
+          </div>
+        </div>
+      `;
+    });
+  
+    mostrarValores.innerHTML = valoresSeleccionados;
 }
