3fff0de17b2e8522906a8cdb29fc06a142e1d551
sebamarques <sebamarquezblanco@gmail.com>
2025-01-20 19:39:21 -0300

    newTry

--- Practica3/tip-calculator-app-main/script.js
+++ Practica3/tip-calculator-app-main/script.js
@@ -1,49 +1,80 @@
-let totalAmount = document.getElementById("amount");
-let customTip = document.getElementById("custom");
-let numberOfPeople = document.getElementById("numberPeople");
-let totalPerson = document.getElementById("totalPerson");
-let totalTip = document.getElementById("tipPerson");
+const billToPay = document.getElementById("bill");
+const customTip = document.getElementById("customTip");
+const numberOfClients = document.getElementById("numberClients");
+const tipButtons = document.querySelectorAll("button[data-tip]");
+const tipAmountElement = document.querySelector(".tip-amount");
+const totalAmountElement = document.querySelector(".total-amount");
+const rstbtn = document.getElementById("resetbtn");
 
-let amount = 0;
-let people = 0;
-let tip = 0;
-let agregarTip = ``;
-let agregarTotal = ``;
+let billValue = 0;
+let tipValue = 0;
+let clientsValue = 0;
 
-function calculate(){
+let totalToPay = 0;
+let totalTip = 0;
+
+document.addEventListener("DOMContentLoaded",function(){
+
+    billToPay.addEventListener("input",function(){
+        calculateTip();
+    })
+
+    customTip.addEventListener("input",function(){
+        tipValue = parseFloat(customTip.value) || 0;
+        calculateTip()
+    })
+
+    numberOfClients.addEventListener("input",function(){
+        calculateTip()
+    })
+
+    tipButtons.forEach(button=>{
+        button.addEventListener("click",function(event){
+
+            event.preventDefault();
+            tipValue = parseFloat(button.getAttribute("data-tip"));
+            customTip.value = "";
+            calculateTip();
+        })
+    })
+    rstbtn.addEventListener("click",reset)
+})
+
+function calculateTip(){
+
+    billValue = parseFloat(billToPay.value) | 0;
+    clientsValue = parseInt(numberOfClients.value) | 1;
     
-    amount = parseInt(totalAmount.value)|| 0;
-    people = parseInt(numberOfPeople.value)||tipbtn(valor);
-    tip = parseInt(customTip.value)||0;
+    totalTip = (billValue * tipValue / 100)
+    totalToPay = (totalTip + billValue) / clientsValue;
 
-    amountPerPeople = amount / people;
-    tipPerPeople = ((tip * amount) / 100) / people;
-    create(amount,people,tip)
+    updateUi();
 }
-function create(tip,amount,tip){
-    if(amount && people && tip){
-        agregarTip =  `
-            <p>$${tipPerPeople}</p>
-        `;
-        agregarTotal = `
-        <p>$${amountPerPeople}</p>
-        `;
-        totalTip.innerHTML = agregarTip;
-        totalPerson.innerHTML = agregarTotal;
+
+function updateUi(){
+
+    if(billValue && clientsValue && tipValue){
+        
+        tipAmountElement.textContent = `$${(totalTip / clientsValue).toFixed(2)}`;
+        totalAmountElement.textContent = `$${(totalToPay).toFixed(2)}`;
+        
+        rstbtn.disabled = false;
     }
+    else{
+        rstbtn.disabled = true;
+    }
+
 }
 
-function tipbtn(valor){
-    console.log(valor);
-    return valor;
-}
-totalAmount.addEventListener("input",function(){
-    calculate();
-});
-numberOfPeople.addEventListener("input",function(){
-    calculate();
-});
-customTip.addEventListener("input",function(){
-    calculate();
-});
+function reset(){
+    billToPay.value = "";
+    customTip.value = "";
+    numberOfClients.value = "";
+    
+    tipAmountElement.textContent = "$0.00";
+    totalAmountElement.textContent = "$0.00";
 
+    rstbtn.disabled = true;
+
+    calculateTip();
+}
